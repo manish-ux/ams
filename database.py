3be6a2bbe97b67003229f8287ebe559d05d9e6b2
Manish Bidari <manishbidari2017@gmail.com>
2025-03-04 21:46:59 +0545

    create database schema

---
+++ database.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+
+connection_obj = sqlite3.connect("ams.db")
+
+def create_table():
+
+    cursor_obj = connection_obj.cursor()
+
+    # create user table
+    cursor_obj.execute("""
+    CREATE TABLE IF NOT EXISTS user (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password BLOB  NOT NULL,
+        gender TEXT NOT NULL,
+        address TEXT,
+        role TEXT CHECK(role IN ('super_admin','artist_manager','artist')) NOT NULL,
+        created_at TEXT,
+        updated_at TEXT                      
+    );
+    """)
+
+    # create Artist table
+    cursor_obj.execute("""
+    CREATE TABLE IF NOT EXISTS artist (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        dob TEXT,
+        gender TEXT NOT NULL,
+        address TEXT,
+        first_release_year INTEGER NOT NULL,
+        no_of_albums_released INTEGER,
+        created_at TEXT,
+        updated_at TEXT                 
+    );
+    """)
+
+    # create Song table
+    cursor_obj.execute("""
+    CREATE TABLE IF NOT EXISTS song (
+        artist_id INTEGER NOT NULL,
+        title TEXT NOT NULL,
+        album_name TEXT,
+        genre TEXT,
+        created_at TEXT,
+        updated_at TEXT,
+        FOREIGN KEY(artist_id) REFERENCES artist(id)                         
+    );
+    """)
+
+    connection_obj.commit()
+    connection_obj.close()
+
+if __name__ == "__main__":
+    create_table()
